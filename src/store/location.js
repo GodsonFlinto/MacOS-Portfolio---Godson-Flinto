54682f57ef67611e68a7f6dd47681a2e8ae76c39
GodsonFlinto <153415803+GodsonFlinto@users.noreply.github.com>
2025-11-29 23:06:42 +0530

    added store for location purpose

---
+++ src/store/location.js
@@ -0,0 +1,18 @@
+const { locations } = require("#constants");
+const { create } = require("zustand");
+const { immer } = require("zustand/middleware/immer");
+
+const DEFAULT_LOCATION = locations.work
+
+useLocationStore = create(immer((set) => ({
+    activeLocation : DEFAULT_LOCATION,
+
+    setActiveLocation : (location = null) => 
+        set((state) => {
+            state.activeLocation = location
+        }),
+
+    resetActiveLocation : () => set((state) => {
+        state.activeLocation = DEFAULT_LOCATION
+    })
+})))
