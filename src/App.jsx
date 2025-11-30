629f0e7a5ebe9eeb20cd526c91f9318d38c4ebd9
GodsonFlinto <153415803+GodsonFlinto@users.noreply.github.com>
2025-11-30 15:41:15 +0530

    added gallery section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Navbar, Welcome, Dock, Home } from "#components"
-import { Finder, Resume, Safari, Terminal, Text, Image, Contact } from "#windows"
+import { Finder, Resume, Safari, Terminal, Text, Image, Contact, Photos } from "#windows"
 
 import gsap from "gsap"
 import { Draggable } from "gsap/Draggable"
@@ -21,6 +21,7 @@ const App = () => {
       <Text />
       <Image />
       <Contact />
+      <Photos />
     </main>
   )
 }
