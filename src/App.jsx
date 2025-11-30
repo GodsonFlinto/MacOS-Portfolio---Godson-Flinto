642b0f3611e536330a562c638a6058444db62e9d
GodsonFlinto <153415803+GodsonFlinto@users.noreply.github.com>
2025-11-30 09:44:29 +0530

    added contact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Navbar, Welcome, Dock } from "#components"
-import { Finder, Resume, Safari, Terminal, Text, Image } from "#windows"
+import { Finder, Resume, Safari, Terminal, Text, Image, Contact } from "#windows"
 
 import gsap from "gsap"
 import { Draggable } from "gsap/Draggable"
@@ -19,6 +19,7 @@ const App = () => {
       <Finder />
       <Text />
       <Image />
+      <Contact />
     </main>
   )
 }
